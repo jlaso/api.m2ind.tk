cfbcd3f14a2a83f8d477a8979dab1f19e85c2f76
Joseluis Laso <jlaso@joseluislaso.es>
2016-12-28 12:19:40 +0100

    endpoint to update username of winner

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -9,4 +9,33 @@ class ScoresController < ApplicationController
     render json: @scores
   end
 
+
+  # PATCH/PUT /scores/1
+  def update
+
+    begin
+      raise 'game_token is mandatory' unless params[:game_token]
+      raise 'user is mandatory' unless params[:user]
+      game_token = params[:game_token]
+      score_id = params[:id]
+      user = params[:user]
+      @score = Score.find(score_id)
+      raise 'score game not found' unless @score
+      raise 'data mismatch' if @score.game_token != game_token
+
+      if @score.update({:user => user})
+        render json: {
+            :success => true
+        }
+      else
+        render json: @score.errors, status: :unprocessable_entity
+      end
+    rescue Exception => e
+      render json: {
+          :success => false,
+          :error => e.message
+      }, status: :internal_server_error
+    end
+  end
+
 end
